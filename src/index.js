5a5420ec6e93bd344f7df4eeedc6213f9e0b74e9
Behnam Mazaheri <b.mazaheri@solvemate.com>
2020-01-27 09:48:24 +0100

    custom element

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const wrapper = document.createElement('div')
+wrapper.innerHTML = `
+  <style>
+  :host {
+    display: block;
+    position: fixed;
+    right: 100px;
+    bottom: 100px;
+    width: 70px;
+    height: 70px;
+    background-color: yellow;
+  }
+  </style>
+`;
+const template = document.createElement('div')
+wrapper.appendChild(template)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class SMWidget extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: 'open' }).appendChild(wrapper);
+
+    ReactDOM.render(<App />, template);
+  }
+}
+customElements.define('sm-widget', SMWidget);
+
 serviceWorker.unregister();
